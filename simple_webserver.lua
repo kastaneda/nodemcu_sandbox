28e07b1d27d9fed47cd6bbda1b90c23a4e89879b
Dmitry Kolesnikov <kastaneda@gmail.com>
2020-02-12 01:31:51 +0200

    Playing with webserver

--- simple_webserver.lua
+++ simple_webserver.lua
@@ -6,13 +6,21 @@ station_cfg.pwd = "1234567890"
 station_cfg.save = false
 wifi.sta.config(station_cfg)
 
+i = 0
+
 -- a simple HTTP server
 srv = net.createServer(net.TCP)
 srv:listen(80, function(conn)
     conn:on("receive", function(sck, payload)
-        print(payload)
+        if i then
+            gpio.write(4, gpio.LOW);
+        else
+            gpio.write(4, gpio.HIGH);
+        end
+        i = 1 - i;
+        print(payload);
+        print(i);
         sck:send("HTTP/1.0 200 OK\r\nContent-Type: text/html\r\n\r\n<h1> Hello, NodeMCU.</h1>")
     end)
     conn:on("sent", function(sck) sck:close() end)
 end)
-
